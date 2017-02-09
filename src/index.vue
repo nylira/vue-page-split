e43d6114e8c2461c35df69eb81d1ac4e7a8a1e5e
Peng Zhong <peng@nylira.com>
2017-02-09 14:50:03 +0800

    init

---
+++ src/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="ni-page-split">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ni-page-split'
+}
+</script>
+
+<style src="./style.css"></style>
+
