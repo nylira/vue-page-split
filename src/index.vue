d990bba097da6ebbdc48b3e8f0a7412b7570e8b2
Peng Zhong <peng@nylira.com>
2017-11-09 10:24:26 -0500

    update style

--- src/index.vue
+++ src/index.vue
@@ -15,4 +15,32 @@ export default {
 }
 </script>
 
-<style src="./style.css"></style>
+<style lang="stylus">
+@require '~@/styles/variables.styl'
+
+.ni-page-split
+   min-height 75vh
+   display flex
+   flex-flow column nowrap
+
+.ni-page-split-main
+   flex 1
+   background app-bg
+
+@media screen and (min-width: 1024px)
+   .ni-page-split
+      flex-flow row nowrap
+      align-items stretch
+   .ni-page-split-header
+      flex 0 0 33.3vw
+      margin-bottom 0
+      border-bottom none
+   .ni-page-split-main
+      flex 0 0 66.7vw
+
+@media screen and (min-width: 1280px)
+   .ni-page-split-header
+      flex 0 0 25vw
+   .ni-page-split-main
+      flex 0 0 75vw
+</style>
